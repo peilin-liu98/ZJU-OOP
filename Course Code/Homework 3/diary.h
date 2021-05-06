47205bd872e962a31b380dc81d54988dfae733a4
Naruto9811 <peilin_liu@foxmail.com>
2021-05-06 11:32:51 +0800

    Code Sturcture

---
+++ Course Code/Homework 3/diary.h
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <sstream>
+#include <set>
+#include <map>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Diary{
+    public:
+    int search_date(int time);
+    void add_diary(int time, vector<string> & text);  //* 同时修改map与set
+    vector<int> list_date(int begin = 0, int end = 99999999);
+    void show(int time);
+    int rm(int time);
+
+    private:
+    map<int, vector<string> > content;
+    set<int> date;
+};
+
+
+int string2time(string line){
+
+    stringstream t(line);
+    vector<int> date;
+    int time = 0;
+    
+    while(t >> time)
+        date.push_back(time);
+
+    time = date[0] * 10000 + date[1] * 100 + date[2];
+    return time;
+
+};
+
+void read_memory(Diary &d){
+
+    string line;
+    int time = 0;
+    vector<string> temp;
+
+    while(getline(cin,line))
+    {
+        time = string2time(line);
+        while(getline(cin,line)){
+            if (line.empty() == true){
+                d.add_diary(time, temp);
+                break;
+            }
+            else{
+                temp.push_back(line);
+            };
+        };    
+    };
+};
+
+void output(Diary &d){
+    
+    string time;
+    vector<int> date(d.list_date());
+    for(auto ptr=date.cbegin(); ptr!=date.cend(); ptr++){
+        time = to_string(*ptr);
+        cout << time.substr(0,4) << " "
+             << time.substr(4,2) << " "
+             << time.substr(6) << endl;
+        d.show(*ptr);
+    };
+};
+
+
